bab88d69d198e855c40d9914779b96f951c5be4b
Kouhei Sutou <kou@clear-code.com>
2011-02-17 20:40:36 +0900

    remove needless toggle-input-method binding.

--- init.el
+++ init.el
@@ -16,7 +16,6 @@
 (define-key global-map (kbd "C-c i") 'indent-region)      ; インデント
 (define-key global-map (kbd "C-c C-i") 'hippie-expand)    ; 補完
 (define-key global-map (kbd "C-c ;") 'comment-dwim)       ; コメントアウト
-(define-key global-map (kbd "C-o") 'toggle-input-method)  ; 日本語入力切替
 (define-key global-map (kbd "M-C-g") 'grep)               ; grep
 (define-key global-map (kbd "C-[ M-C-g") 'goto-line)      ; 指定行へ移動
 
