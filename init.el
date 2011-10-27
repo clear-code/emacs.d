3d8e3acb70f28d2e7459f4e8246fc7827ae2b3ed
Kouhei Sutou <kou@clear-code.com>
2011-10-27 13:04:54 +0900

    use whitespace.el.

--- init.el
+++ init.el
@@ -78,8 +78,20 @@
 
 
 ;;; 空白
-;; 行末の空白を表示
-(setq-default show-trailing-whitespace t)
+;; 2011-10-27
+;; 空白や長すぎる行を視覚化する。
+(require 'whitespace)
+;; 1行が80桁を超えたら長すぎると判断する。
+(setq whitespace-line-column 80)
+(setq whitespace-style '(face              ; faceを使って視覚化する。
+                         trailing          ; 行末の空白を対象とする。
+                         lines-tail        ; 長すぎる行のうち
+                                           ; whitespace-line-column以降のみを
+                                           ; 対象とする。
+                         space-before-tab  ; タブの前にあるスペースを対象とする。
+                         space-after-tab)) ; タブの後にあるスペースを対象とする。
+;; デフォルトで視覚化を有効にする。
+(global-whitespace-mode)
 
 
 ;;; 位置
