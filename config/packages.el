b923cfdb6cc1fb184b0214bc50493d9f30a90a68
Kouhei Sutou <kou@clear-code.com>
2012-03-16 09:42:11 +0900

    el-get: install synchronously

--- config/packages.el
+++ config/packages.el
@@ -3,11 +3,11 @@
 ;; 2012-03-15
 (add-to-list 'load-path "~/.emacs.d/el-get/el-get")
 (unless (require 'el-get nil t)
-  (url-retrieve
-   "https://raw.github.com/dimitri/el-get/master/el-get-install.el"
-   (lambda (status)
-     (end-of-buffer)
-     (eval-print-last-sexp))))
+  (with-current-buffer
+      (url-retrieve-synchronously
+       "https://raw.github.com/dimitri/el-get/master/el-get-install.el")
+    (end-of-buffer)
+    (eval-print-last-sexp)))
 ;; レシピ置き場
 (add-to-list 'el-get-recipe-path
              (concat (file-name-directory load-file-name) "/el-get/recipes"))
