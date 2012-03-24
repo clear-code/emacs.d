3d4bae4f73482b3317a7d94e1153f80c82ccfe05
Kouhei Sutou <kou@clear-code.com>
2012-03-24 23:49:07 +0900

    functions: move find-path-in-parents to functions

--- config/builtins.el
+++ config/builtins.el
@@ -12,19 +12,6 @@
            (magit-status (file-name-directory path))))))
 (define-key dired-mode-map "V" 'dired-vc-status)
 
-;; directoryの中にbase-names内のパスが含まれていたらその絶対パスを返す。
-;; 含まれていなかったらdirectoryの親のディレクトリを再帰的に探す。
-;; 2011-03-19
-(defun find-path-in-parents (directory base-names)
-  (or (find-if 'file-exists-p
-               (mapcar (lambda (base-name)
-                         (concat directory base-name))
-                       base-names))
-      (if (string= directory "/")
-          nil
-        (let ((parent-directory (substring directory 0 -1)))
-          (find-path-in-parents parent-directory base-names)))))
-
 
 ;;; スペルチェック
 ;; 2011-03-09
