a0a93a82f843b053d321a36c23862bb7046783ec
Kouhei Sutou <kou@clear-code.com>
2012-03-24 23:52:16 +0900

    magit: add 'git-' to names

--- config/packages/magit.el
+++ config/packages/magit.el
@@ -4,15 +4,15 @@
 (el-get 'sync '(magit))
 
 ;; コミットメッセージをanythingで挿入できるようにする
-(defvar anything-c-source-commit-messages
-  '((name . "Commit Messages")
-    (candidates . anything-c-commit-messages-candidates)
+(defvar anything-c-source-git-commit-messages
+  '((name . "Git Commit Messages")
+    (candidates . anything-c-git-commit-messages-candidates)
     (action . (("Insert" . (lambda (str) (insert str)))))
     (migemo)
     (multiline))
   "Source for browsing and inserting commit messages.")
 
-(defun anything-c-commit-messages-candidates ()
+(defun anything-c-git-commit-messages-candidates ()
   (let* ((messages-string
           (shell-command-to-string "\\git \\log -50 --format=\"%x00%B\""))
          (raw-messages (string-to-list (split-string messages-string "\0")))
@@ -23,15 +23,15 @@
                  (string-equal message ""))
                messages)))
 
-(defun anything-commit-messages ()
-  "`anything' for commit messages."
+(defun anything-git-commit-messages ()
+  "`anything' for git commit messages."
   (interactive)
-  (anything-other-buffer 'anything-c-source-commit-messages
+  (anything-other-buffer 'anything-c-source-git-commit-messages
                          "*anything commit messages*"))
 
 (defun magit-enable-anything ()
   ;; 過去のコミットメッセージを挿入
-  (define-key magit-log-edit-mode-map (kbd "C-c i") 'anything-commit-messages))
+  (define-key magit-log-edit-mode-map (kbd "C-c i") 'anything-git-commit-messages))
 
 (add-hook 'magit-mode-hook
           'magit-enable-anything)
