16ade3a355c22b9eaf55a2e35e06d0d03835a3b5
Kouhei Sutou <kou@clear-code.com>
2012-03-27 18:57:48 +0900

    flyspell: add comments about configuration

--- config/builtins.el
+++ config/builtins.el
@@ -19,7 +19,9 @@
 ;; M-TABのキーバインドを変更しない
 ;; 2011-03-27
 (setq flyspell-use-meta-tab nil)
+;; デフォルトで自動スペルチェック機能を有効にする
 (setq-default flyspell-mode t)
+;; スペルチェックには英語の辞書を使う
 (setq ispell-dictionary "american")
 
 
