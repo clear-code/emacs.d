79cfd5406b6bf6672ee07e68fa1a1e0b7860253c
Kouhei Sutou <kou@clear-code.com>
2013-01-21 14:33:39 +0900

    rst-mode: remove needless configuration

--- config/packages/rst.el
+++
@@ -1,7 +0,0 @@
-(require 'rst)
-
-(let ((i 1))
-  (while (<= i rst-level-face-max)
-    (let ((face-name (intern (format "rst-level-%d-face" i))))
-      (set-face-background face-name nil)
-      (setq i (1+ i)))))
