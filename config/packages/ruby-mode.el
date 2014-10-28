142b255a1fdfe8b48eadfffcf84bcce0fad0e95b
Kouhei Sutou <kou@clear-code.com>
2014-10-28 23:42:46 +0900

    ruby-mode: use ruby-mode bundled in Emacs
    Because ruby-mode has been removed from the Ruby's repository.

--- config/packages/ruby-mode.el
+++ config/packages/ruby-mode.el
@@ -1,9 +1,13 @@
 ;;; ruby-mode
 ;;; 2012-03-15
 
-;; Emacsにバンドルされているruby-modeは古いのでRubyのリポジ
-;; トリに入っているものを使う。
-(el-get 'sync '(ruby-mode-trunk))
+;; Emacsにバンドルされているruby-modeにはC-c C-eでendを挿入する機能が
+;; なくなっているので、その機能だけ別途追加する。
+;; 2014-10-28
+(defun ruby-insert-end ()
+  "Insert 'end'."
+  (interactive)
+  (insert "end"))
 
 (add-hook 'ruby-mode-hook
           (lambda ()
@@ -14,4 +18,8 @@
             ;; 上書きしてしまうので無効にする。
             ;; 2012-07-11
             (define-key ruby-mode-map (kbd "C-M-p") nil)
-            (define-key ruby-mode-map (kbd "C-M-n") nil)))
+            (define-key ruby-mode-map (kbd "C-M-n") nil)
+
+            ;; C-c C-eで「end」を挿入する
+            ;; 2014-10-28
+            (define-key ruby-mode-map (kbd "C-C C-e") 'ruby-insert-end)))
