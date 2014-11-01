7dcff4df061350f0f035894fc5a7bba070463207
Kouhei Sutou <kou@clear-code.com>
2014-11-01 10:52:43 +0900

    Reduce history size
    Large history size increases history GC time...

--- init.el
+++ init.el
@@ -166,11 +166,11 @@
 
 ;;; 履歴
 ;; 履歴数を大きくする
-(setq history-length 10000)
+(setq history-length 1000)
 ;; ミニバッファの履歴を保存する
 (savehist-mode 1)
 ;; 最近開いたファイルを保存する数を増やす
-(setq recentf-max-saved-items 10000)
+(setq recentf-max-saved-items 1000)
 
 
 ;;; 圧縮
