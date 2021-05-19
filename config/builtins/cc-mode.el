8c4ba787eaeb42b973ffce7578bc24b4a8951eff
Sutou Kouhei <kou@clear-code.com>
2021-05-19 09:33:16 +0900

    cc-mode: don't use hook for EditorConfig

--- config/builtins/cc-mode.el
+++ config/builtins/cc-mode.el
@@ -17,19 +17,45 @@
 ;; C言語と同じような構文のプログラミング言語用の設定
 ;; 2012-03-18
 
+;; hookを利用するとEditorConfigの設定を上書きしてしまうので
+;; スタイルの変更にはhookを使わない。
+;; 2021-05-19
+(require 'cc-mode)
+
+;; BSDスタイルをベースに2スペースでインデントするモードを作る。
+(setq c-style-2-spaces (copy-alist (cdr (assoc "bsd" c-style-alist))))
+;; スペースでインデントをする。
+(setq c-style-2-spaces `((indent-tabs-mode . nil) ,@c-style-2-spaces))
+;; インデント幅を2にする。
+(setcdr (assoc 'c-basic-offset c-style-2-spaces) 2)
+;; C++は開き波括弧の位置を調整する
+;; 定義開始時の開き波かっこ「{」の前で改行しない
+;; デフォルトでは前後に改行が入る
+;; afterを指定すると後だけに改行がはいる
+;; (defun-open before after)にすると前後に改行が入る
+(setq c++-style-2-spaces
+      `((c-hanging-braces-alist . (;; トップレベルの関数定義
+                                   (defun-open after)
+                                   ;; クラス定義
+                                   (class-open after)
+                                   ;; クラス定義内のインラインメソッド定義
+                                   (inline-open after)))
+        ,@c-style-2-spaces))
+;; スタイルを登録する。
+(setq c-style-alist
+      `(("2-spaces-c" ,@c-style-2-spaces)
+        ("2-spaces-c++" ,@c++-style-2-spaces)
+        ,@c-style-alist))
+;; デフォルトで2スペースでインデントするスタイルを使う。
+(setq c-default-style
+      `((c-mode . "2-spaces-c")
+        (c++-mode . "2-spaces-c++")
+        ,@c-default-style))
+
 ;; c-modeやc++-modeなどcc-modeベースのモード共通の設定
 (add-hook
  'c-mode-common-hook
  (lambda ()
-   ;; BSDスタイルをベースにする
-   (c-set-style "bsd")
-
-   ;; スペースでインデントをする
-   (setq indent-tabs-mode nil)
-
-   ;; インデント幅を2にする
-   (setq c-basic-offset 2)
-
    ;; 自動改行（auto-new-line）と
    ;; 連続する空白の一括削除（hungry-delete）を
    ;; 有効にする
@@ -40,18 +66,3 @@
    ;; EmacsFrameClass   => Emacs Frame Class
    ;; NSGraphicsContext => NS Graphics Context
    (subword-mode 1)))
-
-;; c++-modeだけの設定
-;; 2013-01-05
-(add-hook
- 'c++-mode-hook
- (lambda ()
-   ;; 定義開始時の開き波かっこ「{」の前で改行しない
-   ;; デフォルトでは前後に改行が入る
-   ;; afterを指定すると後だけに改行がはいる
-   ;; (defun-open before after)にすると前後に改行が入る
-   (setq c-hanging-braces-alist
-         `((defun-open after)   ; トップレベルの関数定義
-           (class-open after)   ; クラス定義
-           (inline-open after)  ; クラス定義内のインラインメソッド定義
-           ,@c-hanging-braces-alist))))
